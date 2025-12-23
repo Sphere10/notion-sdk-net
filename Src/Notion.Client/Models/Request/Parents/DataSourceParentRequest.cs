9fb2412ea9686da2251d13bd82e5f9be46c8f256
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2025-12-23 17:41:47 +0530

    Add DatabaseId property to DataSourceParentRequest model & Implement IParentOfDataSourceRequest 🏷️

--- Src/Notion.Client/Models/Request/Parents/DataSourceParentRequest.cs
+++ Src/Notion.Client/Models/Request/Parents/DataSourceParentRequest.cs
@@ -3,13 +3,16 @@ using Newtonsoft.Json;
 
 namespace Notion.Client
 {
-    public class DataSourceParentRequest : IParentOfPageRequest
+    public class DataSourceParentRequest : IParentOfPageRequest, IParentOfDataSourceRequest
     {
         [JsonProperty("type")]
         public string Type => "data_source_id";
 
         [JsonProperty("data_source_id")]
         public string DataSourceId { get; set; }
+
+        [JsonProperty("database_id")]
+        public string DatabaseId { get; set; }
 
         /// <summary>
         /// Additional data for future compatibility
