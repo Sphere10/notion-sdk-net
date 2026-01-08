4409cbe6fe95be767b1dc51e86f44aa6335707f3
Herman Schoenfeld <herman@sphere10.com>
2026-01-08 23:44:59 +1000

    Fixed JSON Serialization bugs with IQueryDataSourceResponseObject and IPageCover.

--- Src/Notion.Client/Api/DataSources/Query/Response/IQueryDataSourceResponseObject.cs
+++ Src/Notion.Client/Api/DataSources/Query/Response/IQueryDataSourceResponseObject.cs
@@ -1,9 +1,9 @@
-using JsonSubTypes;
+﻿using JsonSubTypes;
 using Newtonsoft.Json;
 
 namespace Notion.Client
 {
-    [JsonConverter(typeof(JsonSubtypes), "type")]
+    [JsonConverter(typeof(JsonSubtypes), "object")]
     [JsonSubtypes.KnownSubTypeAttribute(typeof(Page), ObjectType.Page)]
     [JsonSubtypes.KnownSubTypeAttribute(typeof(DataSource), ObjectType.DataSource)]
     public interface IQueryDataSourceResponseObject : IObject
